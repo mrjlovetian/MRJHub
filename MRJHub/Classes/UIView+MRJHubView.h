f0d73f795d7827d9306112f3ba1f302e44541eff
MRJ <mrjyuhongjiang@gmail.com>
2017-10-25 16:59:18 +0800

    0.1.2.4

--- MRJHub/Classes/UIView+MRJHubView.h
+++ MRJHub/Classes/UIView+MRJHubView.h
@@ -12,22 +12,16 @@
 
 /// 显示普通的文本信息
 - (void)MRJ_showMessage:(NSString *)msg;
-
 /// 延时显示
 - (void)MRJ_showMessage:(NSString *)msg withDur:(NSTimeInterval)dur;
-
 /// 是否可编辑状态
 - (void)MRJ_showMessage:(NSString *)msg widthDur:(NSTimeInterval)dur edit:(BOOL)edit;
-
 /// 默认加载
 - (void)MRJ_showDefaultAnimationLoading;
-
 /// 动画加载
 - (void)MRJ_showtAnimationLoading;
-
 /// 自定义加载
 - (void)MRJ_showLoadingCustomView:(UIView *)customView toView:(UIView *)toView;
-
 /// 隐藏加载框
 - (void)MRJ_hideView;
 
